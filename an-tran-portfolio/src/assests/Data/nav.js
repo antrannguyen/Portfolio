2d4a3f6ab309ceb2b8fee8899abcf77a9fc261cb
An Tran <thuyan1149@gmail.com>
2020-12-14 01:57:08 +0200

    clean folder

--- an-tran-portfolio/src/assests/Data/nav.js
+++
@@ -1,8 +0,0 @@
-const nav1 = [
-  { link: 'http://localhost:3000/', label: 'About', id: 1 },
-  { link: 'http://localhost:3000/', label: 'Resume', id: 2 },
-  { link: 'http://localhost:3000/', label: 'Project', id: 3 },
-  { link: 'http://localhost:3000/', label: 'Blogs', id: 4 },
-  { link: 'http://localhost:3000/', label: 'Contact', id: 5 },
-];
-export default nav1;
